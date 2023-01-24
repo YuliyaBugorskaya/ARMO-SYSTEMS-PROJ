cfb134860ef84751a010f80973ea84460145c20f
Yuliya <galichina@mail.ru>
2023-01-24 15:19:57 +0300

    initial Project

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage';
+
+function App() {
+  return (
+    <Routes>
+
+      <Route path="/" element={<MainPage />} />
+
+    </Routes>
+  );
+}
+export default App;
